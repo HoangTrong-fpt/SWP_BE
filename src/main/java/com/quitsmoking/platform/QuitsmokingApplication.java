6617cc917fed3520f1c47363f014c6e11b044304
CaoThien <caothien0502@gmail.com>
2025-06-16 00:44:45 +0700

    Update latest changes

--- src/main/java/com/quitsmoking/platform/QuitsmokingApplication.java
+++ src/main/java/com/quitsmoking/platform/QuitsmokingApplication.java
@@ -1,4 +1,4 @@
-package com.quitsmoking.platform.quitsmoking;
+package com.quitsmoking.platform;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
