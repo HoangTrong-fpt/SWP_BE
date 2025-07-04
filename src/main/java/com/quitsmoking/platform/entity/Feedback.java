92cc887b7998ce86bd2d1b85b55c67d5888029b0
HoangTrongfpt <email@cua-ban.com>
2025-07-04 12:53:33 +0700

    Fix FeedBack Get

--- src/main/java/com/quitsmoking/platform/entity/Feedback.java
+++ src/main/java/com/quitsmoking/platform/entity/Feedback.java
@@ -2,11 +2,7 @@ package com.quitsmoking.platform.entity;
 
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
+import lombok.*;
 import java.time.LocalDateTime;
 
 @Entity
