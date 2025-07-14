608cf81be21ce762c6d42f2e0e49861f5226fb9e
HoangTrongfpt <email@cua-ban.com>
2025-07-14 21:55:18 +0700

    bell

--- src/main/java/com/quitsmoking/platform/repository/NotificationRepository.java
+++ src/main/java/com/quitsmoking/platform/repository/NotificationRepository.java
@@ -6,5 +6,6 @@ import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
     List<Notification> findByRecipientId(Long recipientId);
+    int countByRecipientIdAndIsReadFalse(Long recipientId);
 }
 
